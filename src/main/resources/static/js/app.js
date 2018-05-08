7c5157fde94bbc9d6711499e9b34aeff93bfdb01
arpit <itsarpitgupta@gmail.com>
2018-05-08 08:57:26 +0530

    photos added in some products

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,3 +7,17 @@ function validate() {
 		return true;
 	}
 }
+
+function switchTab(type){
+	if($(type).html() == 'Specifications'){
+		$("#pdf-li").removeAttr("class");
+		$("#spec-li").attr("class","active");
+		$("#shop-tab2").css("display", "none");
+		$("#shop-tab1").css("display", "block");
+	}else{
+		$("#spec-li").removeAttr("class");
+		$("#pdf-li").attr("class","active");
+		$("#shop-tab1").css("display", "none");
+		$("#shop-tab2").css("display", "block");
+	}
+}
