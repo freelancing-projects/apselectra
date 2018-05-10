085b0fb9120aa3e417eb7d1dcf2d04a5e52cd7e5
arpit <itsarpitgupta@gmail.com>
2018-05-10 23:51:07 +0530

    home page updated

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -21,3 +21,73 @@ function switchTab(type){
 		$("#shop-tab2").css("display", "block");
 	}
 }
+
+function reason_onmousehover(no){
+	if(no == 'flag'){
+		$("#wcu_DIV_56").css("background" , "#8ec36c");
+		$("#ricon").removeClass();
+		$("#ricon").addClass("fa fa-flag");
+		$("#wcu_H3_71").html('The Best Solar LED Light');
+		$("#wcu_DIV_72").html('Exceeding your expectations for reliability is part of our mission.');
+	}else if(no == 'gears'){
+		$("#wcu_DIV_38").css("background" , "#8ec36c");
+		$("#ricon").removeClass();
+		$("#ricon").addClass("fa fa-gears");
+		$("#wcu_H3_71").html('Options');
+		$("#wcu_DIV_72").html('Our LED lights come in lumen ranges from 4400- 14300. We can even double up a pole to 28,600 lumens.');
+	}
+	else if(no == 'user'){
+		$("#wcu_DIV_29").css("background" , "#8ec36c");
+		$("#ricon").removeClass();
+		$("#ricon").addClass("fa fa-user");
+		$("#wcu_H3_71").html('LED Street Lighting Solutions');
+		$("#wcu_DIV_72").html('If you are looking to light up city roads, a main highway or other large areas, Solar Lighting International’s LED Lighting Systems are a reliable and effective choice.');
+	}
+	else if(no == 'briefcase'){
+		$("#wcu_DIV_20").css("background" , "#8ec36c");
+		$("#ricon").removeClass();
+		$("#ricon").addClass("fa fa-briefcase");
+		$("#wcu_H3_71").html('Industry Experience');
+		$("#wcu_DIV_72").html('With over 40 years of combined solar experience, SLI has the know-how to get the project done right.');
+	}
+	else if(no == 'comments'){
+		$("#wcu_DIV_47").css("background" , "#8ec36c");
+		$("#ricon").removeClass();
+		$("#ricon").addClass("fa fa-comments");
+		$("#wcu_H3_71").html('Complex Solutions');
+		$("#wcu_DIV_72").html('Our LED lights come in lumen ranges from 4400- 14300. We can even double up a pole to 28,600 lumens.');
+	}
+	
+	
+}
+
+jQuery(".info-c-full-br").each(function() {
+    "click" == jQuery(this).data("focus-on") && jQuery(this).find(".icon-circle-list .info-circle-icons").click(function() {
+        var a = jQuery(this);
+        jQuery(this).parents(".info-c-full-br").attr("data-slide-true", "false"),
+        show_next_info_circle(a)
+    }),
+    "hover" == jQuery(this).data("focus-on") && jQuery(this).find(".icon-circle-list .info-circle-icons").hover(function() {
+        var a = jQuery(this);
+        jQuery(this).parents(".info-c-full-br").attr("data-slide-true", "false"),
+        show_next_info_circle(a)
+    }, function() {})
+})
+
+function reason_onmouseout(no){
+	if(no == 'flag'){
+		$("#wcu_DIV_56").css("background", "#f6b435");
+	}else if(no == 'gears'){
+		$("#wcu_DIV_38").css("background", "#f6b435");
+ 	}
+	else if(no == 'user'){
+		$("#wcu_DIV_29").css("background", "#f6b435");
+  	}
+	else if(no == 'briefcase'){
+		$("#wcu_DIV_20").css("background", "#f6b435");
+	}
+	else if(no == 'comments'){
+		$("#wcu_DIV_47").css("background", "#f6b435");
+	}
+	
+}
